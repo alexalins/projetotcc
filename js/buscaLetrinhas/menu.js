eb752736414979d96b8da3cd371483b87745445f
alexaglins <alexa_ip@hotmail.com>
2019-04-18 22:37:02 -0300

    mudando nome do jogo

--- js/buscaLetrinhas/menu.js
+++ js/buscaLetrinhas/menu.js
@@ -1,6 +1,6 @@
 var menuState = {
     create: function(){
-        var txtQuiz = game.add.text(game.world.centerX, 150, 'BUSCA PALAVRAS', {font:'40px emulogic', fill:'#fff'});
+        var txtQuiz = game.add.text(game.world.centerX, 150, 'BUSCA LETRINHAS', {font:'40px emulogic', fill:'#fff'});
         txtQuiz.anchor.set(.5);
         //
         var botaoPlay = game.add.button(game.world.centerX - 95, 300, 'botao', play, this, 2, 1, 0);
